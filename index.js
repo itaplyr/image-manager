0c34fcec9257f38c6dbf4753a9a3578a907772dd
itaplyr <itaplyr@gmail.com>
2026-02-01 12:08:03 +0100

    uptaded cache time

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const WORKER_URLS = (process.env.WORKER_URLS || 'http://localhost:3001').split(',');
 const POLL_INTERVAL = 60 * 1000;
 const IMAGE_DIR = path.join(process.cwd(), 'images');
-const CACHE_DURATION = 24 * 60 * 60 * 1000;
+const CACHE_DURATION = 10 * 60 * 1000;
 
 if (!fs.existsSync(IMAGE_DIR)) {
     fs.mkdirSync(IMAGE_DIR, { recursive: true });
